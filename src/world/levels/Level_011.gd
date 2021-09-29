11010a9ae2aeba0988748cbff045077ce4fdbec8
Dark-Gran <filip.randak@gmail.com>
2021-09-29 20:11:29 +0200

    level 11 draft (old level 11 now level 12 + rotatable rotation-direction fix + new walls)

--- src/world/levels/Level_011.gd
+++ src/world/levels/Level_011.gd
@@ -1,18 +1,7 @@
 extends Level
 
 func _ready():
-	# Mid
+	$Statics/Button.switchables.append($Statics/Rotatables/RotatableTriangle)
+	$Statics/Button.switchables.append($Statics/Rotatables/RotatableTriangle2)
 	$Statics/Button.switchables.append($Statics/Rotatables/RotatableTriangle3)
 	$Statics/Button.switchables.append($Statics/Rotatables/RotatableTriangle4)
-	$Statics/Button.switchables.append($Statics/Rotatables/RotatableTriangle9)
-	$Statics/Button.switchables.append($Statics/Rotatables/RotatableTriangle10)
-	# Bottom
-	$Statics/Button2.switchables.append($Statics/Rotatables/RotatableTriangle8)
-	$Statics/Button2.switchables.append($Statics/Rotatables/RotatableTriangle9)
-	$Statics/Button2.switchables.append($Statics/Rotatables/RotatableTriangle10)
-	$Statics/Button2.switchables.append($Statics/Rotatables/RotatableTriangle11)
-	# Top
-	$Statics/Button3.switchables.append($Statics/Rotatables/RotatableTriangle2)
-	$Statics/Button3.switchables.append($Statics/Rotatables/RotatableTriangle3)
-	$Statics/Button3.switchables.append($Statics/Rotatables/RotatableTriangle4)
-	$Statics/Button3.switchables.append($Statics/Rotatables/RotatableTriangle5)
