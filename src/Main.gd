247aaa714589fea45bc69e3b6e64d08708b756a2
Dark-Gran <filip.randak@gmail.com>
2022-06-09 02:40:02 +0200

    enabled intro scene

--- src/Main.gd
+++ src/Main.gd
@@ -10,7 +10,7 @@ var current_scene = null
 func _ready():
 	var root = get_tree().get_root()
 	current_scene = root.get_child(root.get_child_count() - 1)
-	goto_scene("res://src/world/World.tscn")
+	goto_scene("res://src/intro/Intro.tscn")
 
 func goto_scene(path):
 	call_deferred("_deferred_goto_scene", path)
